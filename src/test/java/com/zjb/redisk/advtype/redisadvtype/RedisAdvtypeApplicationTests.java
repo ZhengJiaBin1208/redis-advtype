8f092294527ab292355a36d402170d017010d01d
ZhengJiaBin1208 <zhengjiabin@bestseller.com.cn>
2024-06-07 11:56:51 +0800

    Initial commit

---
+++ src/test/java/com/zjb/redisk/advtype/redisadvtype/RedisAdvtypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjb.redisk.advtype.redisadvtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisAdvtypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
